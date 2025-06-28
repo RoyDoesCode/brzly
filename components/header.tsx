5f0a04ef8ed8891ce1d4116fb22818bfb76eadf8
RoyDoesCode <roybar03@gmail.com>
2025-06-28 14:19:43 +0300

    .

--- components/header.tsx
+++ components/header.tsx
@@ -89,18 +89,18 @@ export default function Header() {
                             </div>
                             <div className="flex w-full flex-col space-y-3 sm:flex-row sm:gap-3 sm:space-y-0 md:w-fit">
                                 <Button asChild variant="outline" size="sm" className={cn(isScrolled && "lg:hidden")}>
-                                    <Link href="/#testimonials">
-                                        <span>{t("testimonials")}</span>
+                                    <Link href="/contact">
+                                        <span>{t("contact")}</span>
                                     </Link>
                                 </Button>
                                 <Button asChild size="sm" className={cn(isScrolled && "hidden")}>
-                                    <Link href="/contact">
-                                        <span>{t("contact")}</span>
+                                    <Link href="/apply">
+                                        <span>{t("apply")}</span>
                                     </Link>
                                 </Button>
                                 <Button asChild size="sm" className={cn(isScrolled ? "lg:inline-flex" : "hidden")}>
-                                    <Link href="/contact">
-                                        <span>{t("contact")}</span>
+                                    <Link href="/apply">
+                                        <span>{t("apply")}</span>
                                     </Link>
                                 </Button>
                             </div>
